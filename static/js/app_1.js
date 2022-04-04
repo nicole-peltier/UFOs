2144728c5a115a28d9c0db04b7b1db2afc745078
nicole-peltier <nicole.peltier3@yahoo.com>
2022-04-04 15:27:19 -0500

    Updating Code

--- static/js/app_1.js
+++ static/js/app_1.js
@@ -42,7 +42,7 @@ function handleClick() {
 }
 
 // Attach an event to listen for the form button
-d3.selectAll("filter-btn").on("click", handleClick);
+d3.selectAll("#filter-btn").on("click", handleClick);
 
 // Build the table when the page loads
 buildTable(tableData);
